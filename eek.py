66e810defddaed0532a138b004739d6bcd6cb8e8
AdwayB <adwaybyju@gmail.com>
2023-06-05 17:12:13 +0530

    Optimised Detection

--- eek.py
+++ eek.py
@@ -4,7 +4,7 @@ import time
 
 # Load pre-trained models
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-hand_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_hand.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
 
 
 # Function to detect faces and hands
@@ -14,28 +14,28 @@ def detect_faces_hands(frame):
     # Detect faces
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Detect hands
-    hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
+    # Detect eyes
+    eyes = eye_cascade.detectMultiScale(gray, 1.3, 6)
 
     # Draw rectangles around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    # Draw rectangles around the detected hands
-    for (x, y, w, h) in hands:
+    # Draw rectangles around the detected eyes
+    for (x, y, w, h) in eyes:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    return frame, len(faces) > 0, len(hands) > 0
+    return frame, len(faces) > 0, len(eyes) > 0
 
 
 # Open webcam
 cap = cv2.VideoCapture(0)
 total_face = 0
-total_hand = 0
+total_eyes = 0
 face_start_time = None
 face_duration = 0
-hand_start_time = None
-hand_duration = 0
+eyes_start_time = None
+eyes_duration = 0
 looking_at_camera = False
 
 while True:
@@ -46,42 +46,42 @@ while True:
     if not ret:
         break
 
-    # Detect faces and hands in the frame
-    output_frame, face_detected, hand_detected = detect_faces_hands(frame)
+    # Detect faces and gaze in the frame
+    output_frame, face_detected, eyes_detected = detect_faces_hands(frame)
 
     # Update face duration
     if face_detected:
         if face_start_time is None:
             face_start_time = time.time()
         face_duration = time.time() - face_start_time
-    else:
         total_face += face_duration
+    else:
         face_start_time = None
         face_duration = 0
 
-    # Update hand duration
-    if hand_detected:
-        if hand_start_time is None:
-            hand_start_time = time.time()
-        hand_duration = time.time() - hand_start_time
+    # Update gaze duration
+    if eyes_detected:
+        if eyes_start_time is None:
+            eyes_start_time = time.time()
+        eyes_duration = time.time() - eyes_start_time
+        total_eyes += eyes_duration
     else:
-        total_hand += hand_duration
-        hand_start_time = None
-        hand_duration = 0
+        eyes_start_time = None
+        eyes_duration = 0
 
     # Display the output frame
-    cv2.imshow('Face and Hand Detection', output_frame)
+    cv2.imshow('Face and Eye Detection', output_frame)
 
-    # Log the duration of face, hands, and gaze
+    # Log the duration of face, gaze
     print("Face duration: {:.2f}s".format(face_duration))
-    print("Hand duration: {:.2f}s".format(hand_duration))
+    print("Eye duration: {:.2f}s".format(eyes_duration))
 
-    # Break the loop if 'q' is pressed
+    # Break the loop if 's' is pressed
     if cv2.waitKey(1) & 0xFF == ord('s'):
         break
 
-print("Total Face duration: {:.2f}s".format(total_face))
-print("Total Hand duration: {:.2f}s".format(total_hand))
+print("\n\nTotal Face duration: {:.2f}s".format(total_face))
+print("Total Gaze duration: {:.2f}s".format(total_eyes))
 # Release the webcam and close all windows
 cap.release()
 cv2.destroyAllWindows()
